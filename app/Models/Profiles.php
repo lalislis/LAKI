a825c564e2059fd587c91a4a058756845486aa43
elseptiawan <septiawanelshandi@gmail.com>
2022-04-10 10:42:16 +0700

    Edit Profiled Model

--- app/Models/Profiles.php
+++ app/Models/Profiles.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Profiles extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function company(){
+        return $this->belongsTo(Companies::class);
+    }
+
+    public function media(){
+        return $this->belongsTo(Media::class);
+    }
 }
