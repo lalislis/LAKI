083bf5a049b240c2343fe819714f5e326e44f431
elseptiawan <septiawanelshandi@gmail.com>
2022-04-10 12:04:46 +0700

    Edit Profiles Controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Profiles;
 
 class ProfilesController extends Controller
 {
-    //
+    public function index(){
+        $profiles = Profiles::all();
+        return $profiles;
+    }
+
+    public function show(Profiles $profile){
+        return $profil;
+    }
 }
