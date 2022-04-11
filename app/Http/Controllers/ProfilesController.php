8ef63d32c8eea58e37182c9b4afa6ca0e99376df
elseptiawan <septiawanelshandi@gmail.com>
2022-04-11 19:39:21 +0700

    Update Profiles Controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Profiles;
+use App\Models\Media;
 
 class ProfilesController extends Controller
 {
@@ -13,6 +14,26 @@ class ProfilesController extends Controller
     }
 
     public function show(Profiles $profile){
+        return $profil;
+    }
+
+    public function update(Request $request, Profiles $profile){
+        $this->validate($request, [
+            'company_id' => 'required',
+            'media_id' => 'required',
+            'name' => 'required',
+            'position' => 'required'
+        ]);
+
+        $media = Media::where('user_id', $profile->user_id);
+        $media->storage_path = $request->input('storage_path');
+        $profile->company_id = $request-input('company_id');
+        $profile->media_id = $media->id;
+        $profile->name = $request-input('name');
+        $profile->position = $request-input('position');
+        $media->update();
+        $profile->update();
+
         return $profil;
     }
 }
