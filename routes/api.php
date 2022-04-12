283d7ebc92c85c994504146aa9ad123434818e83
elseptiawan <septiawanelshandi@gmail.com>
2022-04-12 16:25:14 +0700

    Edit Route API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfilesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/profiles', [ProfilesController::class, 'index']);
+Route::get('/profiles/{profile:id}', [ProfilesController::class, 'show']);
+Route::post('/profiles/update/{profile:id}', [ProfilesController::class, 'update']);
