cedd457213b2f4ed73a45e621f5ad0373d614977
elseptiawan <septiawanelshandi@gmail.com>
2022-04-14 14:19:02 +0700

    Edit Presence Model

--- app/Models/Presences.php
+++ app/Models/Presences.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Presences extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $with = ['media'];
+
+    public function media(){
+        return $this->belongsTo(Media::class);
+    }
 }
