122dce5196a30abcfb792466d5bc47041290d249
elseptiawan <septiawanelshandi@gmail.com>
2022-04-14 14:19:23 +0700

    Edit Presences Controller

--- app/Http/Controllers/PresencesController.php
+++ app/Http/Controllers/PresencesController.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Media;
+use App\Models\Presences;
 
 use Illuminate\Http\Request;
 
 class PresencesController extends Controller
 {
-    //
+    public function store(Request $request, User $user){
+        $request->validate([
+            'foto' => 'required'
+        ]);
+
+        $foto = $request->file('foto');
+        
+        $media = Media::create([
+            'user_id' => $user->id,
+            'text' => ' ',
+            'storage_path' => $foto->getClientOriginalName()
+        ]);
+
+        $presence = Presences::create([
+            'user_id' => $user->id,
+            'media_id' => $media->id
+        ]);
+        
+        $data = $presence->where('id', $presence->id)->with('media')->get();
+
+        return $data;
+        
+    }
 }
