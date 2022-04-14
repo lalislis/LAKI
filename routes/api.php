efa3c0a0d103221fead57ce110eb6e82c59d25f3
elseptiawan <septiawanelshandi@gmail.com>
2022-04-14 14:19:32 +0700

    Edit Route API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfilesController;
+use App\Http\Controllers\PresencesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/profiles', [ProfilesController::class, 'index']);
 Route::get('/profiles/{profile:id}', [ProfilesController::class, 'show']);
 Route::post('/profiles/update/{profile:id}', [ProfilesController::class, 'update']);
+
+Route::post('/presence/{user:id}', [PresencesController::class, 'store']);
