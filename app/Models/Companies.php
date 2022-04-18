bb4cb3668972078873b2680501157d3ba4cc6478
lalislis <realihsan02@gmail.com>
2022-04-18 18:10:34 +0700

    Add SoftDeletes

--- app/Models/Companies.php
+++ app/Models/Companies.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Model, SoftDeletes};
 
 class Companies extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
