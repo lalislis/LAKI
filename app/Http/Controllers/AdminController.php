5b4bad69b9acf2520921c1b93769b35af3901a03
elseptiawan <septiawanelshandi@gmail.com>
2022-04-23 01:43:51 +0700

    Add and Edit Admin Controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Profiles;
+use App\Models\Companies;
+
+class AdminController extends Controller
+{
+    public function showSuperUser(){
+        $superUser = User::where('role', '3')->with('profile')->get();
+
+        return $superUser;
+    }
+
+    public function showCompanies(){
+        $companies = Companies::all();
+
+        return $companies;
+    }
+
+    public function registerSuperUser(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'title' => 'required',
+            'company' => 'required',
+            'password' => 'required',
+            'confirm_password' => 'required_with:password|same:password'
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => $request->password,
+            'role' => "Super User"
+        ]);
+
+        $profile = Profiles::create([
+            'name' => $request->name,
+            'position' => $request->title,
+            'company_id' => $request->company,
+            'user_id' => $user->id,
+            'media_id' => "1"
+        ]);
+
+        $data = Profiles::where('id', $profile->id)->first();
+
+        return $data;
+    }
+
+    public function registerCompany(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+            'website' => 'required'
+        ]);
+
+        $company = Companies::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'website' => $request->website
+        ]);
+
+        $data = Companies::where('id', $company->id)->first();
+
+        return $data;
+    }
+}
