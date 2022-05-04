d8a36d7d1ee6bd498f5853fcee90485b2f1b336e
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-05-04 21:40:07 +0700

    Add more validation and throw error validation

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,9 +12,10 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    public function getSuperUser(){
-        $superUser = User::where('role', '2')->with('profile','tasks')->get();
-        
+    public function getSuperUser()
+    {
+        $superUser = User::where('role', '2')->with('profile', 'tasks')->get();
+
         return response()->json([
             'success' => true,
             'messages' => 'Data retrieved succesfully',
@@ -22,18 +23,25 @@ class AdminController extends Controller
         ]);
     }
 
-    public function registerSuperUser(Request $request){
-        $req = $request->all();
-
-        $validator = Validator::make($req, [
-            'name' => ['required', 'string'],
-            'email' => ['required', 'string'],
-            'position' => ['required', 'string'],
-            'password' => ['required', 'string'],
-            'confirm_password' => ['required', 'string', 'same:password'],
-            'company_id' => ['required', 'int'],
-            'role' => ['required', 'int']
+    public function registerSuperUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique',
+            'position' => 'required|string',
+            'password' => 'required|string',
+            'confirm_password' => 'required|string|same:password',
+            'company_id' => 'required|integer|exists:companies,id',
+            'role' => 'required|integer|in:1,2,3',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'validations' => $validator->errors(),
+                'data' => ''
+            ], 422);
+        }
 
         $user = User::create([
             'email' => $request->email,
@@ -41,7 +49,7 @@ class AdminController extends Controller
             'role' => $request->role
         ]);
 
-        $profile = Profiles::create([
+        Profiles::create([
             'name' => $request->name,
             'position' => $request->position,
             'company_id' => $request->company_id,
@@ -55,7 +63,8 @@ class AdminController extends Controller
         ]);
     }
 
-    public function deleteSuperUser(User $user){
+    public function deleteSuperUser(User $user)
+    {
         $user->delete();
         return response()->json([
             'success' => true,
@@ -63,20 +72,27 @@ class AdminController extends Controller
         ]);
     }
 
-    public function registerCompany(Request $request){
+    public function registerCompany(Request $request)
+    {
         $data = $request->except('_token', '_method');
-        $req = $request->all();
-
-        $validator = Validator::make($req, [
+        $validator = Validator::make($request->all(), [
             'company_id' => ['required', 'int'],
             'name' => ['required', 'string'],
-            'email' => ['required', 'string'],
+            'email' => ['required', 'string', 'unique'],
             'address' => ['required', 'string'],
             'website' => ['required', 'string'],
-            'phone' => ['required', 'string', 'same:password'],
+            'phone' => ['required', 'string'],
         ]);
 
-        if($request->compani_id == NULL){
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'validations' => $validator->errors(),
+                'data' => ''
+            ], 422);
+        }
+
+        if ($request->compani_id == NULL) {
             $company = Companies::create([
                 'name' => $request->name,
                 'email' => $request->email,
@@ -85,35 +101,36 @@ class AdminController extends Controller
                 'website' => $request->website,
                 'media_id' => '1'
             ]);
-        }
-        else{
+        } else {
             $company = Companies::where('id', $request->company_id)->first();
             $company->update($data);
         }
 
         return response()->json([
             'success' => true,
-            'messages' => 'The company has been created and updated',
+            'messages' => 'The company has been created or updated',
         ]);
     }
 
-    public function listCompanies(){
+    public function listCompanies()
+    {
         $companies = Companies::get();
 
-        $namecompanies = $companies->map(function ($company) {
+        $listCompanies = $companies->map(function ($company) {
             return collect($company->toArray())
-                ->only(['name'])
+                ->only(['id', 'name'])
                 ->all();
         });
 
         return response()->json([
             'success' => true,
             'messages' => 'Data retrieved succesfully',
-            'data' => $namecompanies,
+            'data' => $listCompanies,
         ]);
     }
 
-    public function getCompanies(){
+    public function getCompanies()
+    {
         $companies = Companies::get();
 
         return response()->json([
